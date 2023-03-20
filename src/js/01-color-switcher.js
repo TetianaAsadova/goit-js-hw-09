71afc42a34021969b8d6e8512c02f7d4344dc1e0
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2023-03-20 23:26:37 +0200

    sum1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+import '../css/common.css';
 
+const bodyRef = document.querySelector('body');
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
+
+let intervalId = null;
+let isActive = false;
+
+startBtnRef.addEventListener('click', onStartChangeColorBody);
+stopBtnRef.addEventListener('click', onStopChangeColorBody);
+
+function onStartChangeColorBody(e) {
+    if (isActive) {
+        return;
+    };
+    intervalId = setInterval(() => {
+        bodyRef.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    isActive = true;
+}
+
+function onStopChangeColorBody(e) {
+    isActive = false;
+    clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,10 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import '../css/common.css';
+
+const startBtnRef = document.querySelector('button[data-start]');
+const daysRef = document.querySelector('span[data-days]');
+const hoursRef = document.querySelector('span[data-hours]');
+const minutesRef = document.querySelector('span[data-minutes]');
+const secondsRef = document.querySelector('span[data-seconds]');
+
