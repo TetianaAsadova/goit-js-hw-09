a7c68f315de7ab0d75b15c2032f2847452367d1b
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2023-03-23 12:12:31 +0000

    Deploying to gh-pages from @ TetianaAsadova/goit-js-hw-09@c1c13e8c40427353cb386aa7179502d2fe37de50 🚀

---
+++ 01-color-switcher.1e382bfd.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");let o=null,r=!1;e.addEventListener("click",(function(e){if(r)return;o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),r=!0})),n.addEventListener("click",(function(t){r=!1,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.1e382bfd.js.map
