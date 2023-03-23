c1c13e8c40427353cb386aa7179502d2fe37de50
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2023-03-23 14:11:54 +0200

    sum 7

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,62 +3,37 @@ import '../css/common.css';
 const refs = {
   form: document.querySelector('.form')
 };
-const formObjekt = {
-    delay: "",
-    step: "",
-    amount: "",
-}
+
+let promDelay = 0;
+let promStep = 0;
+let promAmount = 0;
 
 refs.form.addEventListener('submit', (e) => {
   e.preventDefault();
-      const { delay, step, amount } = e.currentTarget.elements;
-      console.log(`delay: ${delay.value}, step: ${step.value}, amount: ${amount.value}`);
-      formObjekt.delay = delay.value;
-      formObjekt.step = step.value;
-      formObjekt.amount = amount.value;
-      console.log(`formObjekt`, formObjekt);
-      // e.currentTarget.reset();
+  const { delay, step, amount } = e.currentTarget.elements;
+  console.log(`delay: ${delay.value}, step: ${step.value}, amount: ${amount.value}`);
+  promDelay = Number(delay.value);
+  promStep = Number(step.value);
+  promAmount = Number(amount.value); 
+
+  for (let i = 1; i <= promAmount; i += 1) {
+    createPromise(i, promDelay)
+      .then(result => console.log(result))
+      .catch(error => console.log(error));
+    promDelay += promStep;
+  } 
 });
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
-
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-
-//   PromiseDelay = delay
-// PromiseDelay += step
-
-// const formRef = document.querySelector('.login-form');
-// const formObjekt = {
-//     email: "",
-//     password: "",
-// }
-
-// formRef.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-//     if (e.currentTarget.email.value === "" || e.currentTarget.password.value === "") {
-//         console.error("Please fill in all the fields!");
-//     } else {
-//         const { email, password } = e.currentTarget.elements;
-//         console.log(`Email: ${email.value}, Password: ${password.value}`);
-//         formObjekt.email = email.value;
-//         formObjekt.password = password.value;
-//         console.log(formObjekt);
-//         e.currentTarget.reset();
-//     }
-// });
+const createPromise = (position, delay) => {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3; 
+    setTimeout(() => {
+      if (shouldResolve) {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, `${promDelay}`);
+  });
+  
+};
